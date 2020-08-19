b67f29febfb5facacdc1c7a98c375371a4fca8bb
KushalSr <kushalsrivastava10@gmail.com>
2020-08-19 20:20:35 +0530

    Adding new Files

---
+++ app.py
@@ -0,0 +1,18 @@
+### One Way
+
+import ecommerce.shipping
+ecommerce.shipping.calc_shipping()
+
+#### Second Way
+
+from ecommerce.shipping import calc_shipping
+
+calc_shipping()
+calc_shipping()
+
+#### Third Way
+
+
+from ecommerce import shipping
+shipping.calc_shipping()
+
